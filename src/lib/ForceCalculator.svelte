ee47a0e1dde111ab5eeef9244d2a56d7c096144f
phuanh004 <phuanh004@gmail.com>
2021-09-28 10:22:20 -0700

    Add calculate the Force

--- src/lib/ForceCalculator.svelte
+++ src/lib/ForceCalculator.svelte
@@ -1,27 +1,131 @@
-<style>
-svg {
-  padding: 50px;
-}
-</style>
-
 <script lang="ts">
 import { Decimal } from "decimal.js";
 
+const COORDINATES_SYSTEM_MAX_LENGTH = 10;
+const SCALE: number = 40;
+const FORCE_SCALE: number = 15000000 * 100; // (2 * 100)
+// let forceScale = 0;
+
 let inputQ1: number = 1;
-let inputQ2: number = 1;
+let inputQ2: number = 3;
 let inputQ3: number = -1;
 
-let forceMagnitude: string = "";
+let q1: Charge, q2: Charge, q3: Charge; // 3 Charges
+$: F1on2 = Number(
+  forceMagnitude(q1, q2, distanceOfTwoPointCharge(q1, q2)).toString()
+).toLocaleString();
+
+// let inputLocationQ1: number =
+
+type Point = {
+  location: Coordinate;
+};
+
+type Line = {
+  p1: Coordinate;
+  p2: Coordinate;
+};
+
+type Charge = Point & {
+  value: number;
+  // location: Coordinate;
+};
+
+type Vector = Line & {
+  magnitude: number;
+  magnitude_scaled?: number;
+};
+
+/**
+ * y = mx + b
+ */
+type LinearEquation = {
+  slope: number;
+  y_int: number;
+};
+
+type Coordinate = {
+  x: number;
+  y: number;
+};
+
+// let forceMagnitude: string = "";
+
+let error = "";
 
 /**
  * Calculate force magnitude between 2 charges
+ *
+ * @param q1  first charge
+ * @param q2  second charge
+ * @param r12 the distance between 2 charges
  */
-const calculateForceMagnitude = (
-  q1: number,
-  q2: number,
-  r12: number
-): Decimal => {
-  return coulombConstant().times(new Decimal((q1 * q2) / (r12 * r12)));
+const forceMagnitude = (q1: Charge, q2: Charge, r12: number): Decimal => {
+  return coulombConstant().times(
+    new Decimal(Math.abs(q1.value * q2.value) / (r12 * r12))
+  );
+};
+
+const force = (
+  q1: Charge,
+  q2: Charge
+): {
+  F1on2: Vector;
+  F2on1: Vector;
+} => {
+  const eq = linearEquation(q1, q2);
+  const r12 = vectorOfTwoPoint(q1, q2).magnitude;
+  const magnitude = forceMagnitude(q1, q2, r12).toNumber();
+  // const base = Decimal.log2(magnitude);
+  const magnitude_scaled = FORCE_SCALE / magnitude;
+
+  let F2on1_x1 = isPush(q1, q2)
+    ? q1.location.x - magnitude_scaled
+    : q1.location.x + magnitude_scaled;
+  let F2on1_x2 = eq.slope * F2on1_x1 + eq.y_int;
+
+  let F1on2_x1 = isPush(q1, q2)
+    ? q2.location.x + magnitude_scaled
+    : q2.location.x - magnitude_scaled;
+  let F1on2_x2 = eq.slope * F1on2_x1 + eq.y_int;
+
+  let F2on1: Vector = {
+    magnitude: magnitude,
+    magnitude_scaled: magnitude_scaled,
+    p1: q1.location,
+    p2: {
+      x: F2on1_x1,
+      y: F2on1_x2,
+    },
+  };
+
+  let F1on2: Vector = {
+    magnitude: magnitude,
+    magnitude_scaled: magnitude_scaled,
+    p1: q2.location,
+    p2: {
+      x: F1on2_x1,
+      y: F1on2_x2,
+    },
+  };
+
+  console.log({
+    F1on2,
+    F2on1,
+    eq,
+  });
+
+  return {
+    F1on2: F1on2,
+    F2on1: F2on1,
+  };
+};
+
+const distanceOfTwoPointCharge = (q1: Charge, q2: Charge): number => {
+  return Math.sqrt(
+    Math.pow(q1.location.x - q2.location.x, 2) +
+      Math.pow(q1.location.y - q2.location.y, 2)
+  );
 };
 
 /**
@@ -38,26 +142,124 @@ const coulombConstant = (): Decimal => {
     .dividedBy(4 * 625000);
 };
 
+const linearEquation = (p1: Point, p2: Point): LinearEquation => {
+  const slope =
+    (p2.location.y - p1.location.y) / (p2.location.x - p1.location.x);
+  return {
+    slope: (p2.location.y - p1.location.y) / (p2.location.x - p1.location.x),
+    y_int: p1.location.y - slope * p1.location.x,
+  };
+};
+
+const vectorOfTwoPoint = (p1: Point, p2: Point): Vector => {
+  const eq = linearEquation(p1, p2);
+  const x1 = p1.location.x;
+  const x2 = p1.location.y;
+  const magnitude = Math.sqrt(
+    Math.pow(p1.location.x - p2.location.x, 2) +
+      Math.pow(p1.location.y - p2.location.y, 2)
+  );
+
+  return {
+    magnitude: magnitude,
+    p1: {
+      x: x1,
+      y: eq.slope * x1 + eq.y_int,
+    },
+    p2: {
+      x: x2,
+      y: eq.slope * x2 + eq.y_int,
+    },
+  };
+};
+
 /**
  *
  * @param input_value
  */
 const onInputSet = (input_value: string) => {
-  if (!input_value) {
+  if (!input_value && !isDataValid(inputQ1, inputQ2, inputQ3)) {
+    error = "Charge cannot equal zero";
     return;
   }
 
-  forceMagnitude = calculateForceMagnitude(inputQ1, inputQ2, inputQ3)
-    .abs()
-    .toString();
+  // forceMagnitude = calculateForceMagnitude(inputQ1, inputQ2, inputQ3)
+  //   .abs()
+  //   .toString();
 
-  forceMagnitude = Number(forceMagnitude).toLocaleString();
+  // forceMagnitude = Number(forceMagnitude).toLocaleString();
 
-  console.log(forceMagnitude);
+  // console.log(forceMagnitude);
 };
 
-// console.log(coulombConstant().toString());
-// console.log(calculateForceMagnitude(1, 3, 4).toString());
+/**
+ * Check data is valid
+ * q1, q2, q3 must be not equal to zero
+ * q1 =/= q2 =/= q3
+ * Two charge
+ *
+ * @param q1 charge 1
+ * @param q2 charge 2
+ * @param q3 charge 3
+ */
+const isDataValid = (q1: number, q2: number, q3: number): boolean => {
+  if (q1 === 0 || q2 === 0 || q3 === 0) return false;
+
+  // TODO: Check 2 charges positive and 1 charge negative
+
+  return q1 !== q2 && q2 !== q3 && q1 !== q3;
+};
+
+/**
+ * Convert cartesian system to SVG system with the scale
+ * @param param0
+ */
+function cartesianToSVG(x: number, y: number) {
+  return {
+    x: x * SCALE,
+    y: y * SCALE,
+  };
+}
+
+const calculateVectorMagnitude = () => {};
+
+const calculateAngle = (q1: Charge, q2: Charge) => {
+  const dx = Math.abs(q1.location.x - q2.location.x);
+  const dy = Math.abs(q1.location.y - q2.location.y);
+  return Math.atan(dy / dx);
+};
+
+const isPush = (q1: Charge, q2: Charge): boolean => {
+  return new Decimal(q1.value * q2.value).isPositive();
+};
+
+const initData = () => {
+  q1 = {
+    value: 3,
+    location: {
+      x: 1,
+      y: 4,
+    },
+  };
+
+  q2 = {
+    value: -1,
+    location: {
+      x: 4,
+      y: 2,
+    },
+  };
+
+  q3 = {
+    value: 2,
+    location: {
+      x: 9,
+      y: 3,
+    },
+  };
+};
+
+initData();
 </script>
 
 <main>
@@ -68,8 +270,27 @@ const onInputSet = (input_value: string) => {
       name="setA"
       type="number"
       placeholder="1"
-      bind:value="{inputQ1}"
+      bind:value="{q1.value}"
       on:input="{onInputSet(inputQ1)}" />
+  </div>
+
+  <div class="set_input_section">
+    <span>Charge 1 location: </span>
+    <label for="charge1_location_x">x: </label>
+    <input
+      id="charge1_location_x"
+      name="charge1_location_x"
+      type="number"
+      placeholder="1"
+      bind:value="{q1.location.x}" />
+
+    <label for="charge1_location_y">y: </label>
+    <input
+      id="charge1_location_y"
+      name="charge1_location_y"
+      type="number"
+      placeholder="1"
+      bind:value="{q1.location.y}" />
   </div>
 
   <div class="set_input_section">
@@ -79,8 +300,27 @@ const onInputSet = (input_value: string) => {
       name="setB"
       type="number"
       placeholder="1"
-      bind:value="{inputQ2}"
+      bind:value="{q2.value}"
       on:input="{onInputSet(inputQ2)}" />
+  </div>
+
+  <div class="set_input_section">
+    <span>Charge 2 location: </span>
+    <label for="charge2_location_x">x: </label>
+    <input
+      id="charge2_location_x"
+      name="charge2_location_x"
+      type="number"
+      placeholder="1"
+      bind:value="{q2.location.x}" />
+
+    <label for="charge2_location_y">y: </label>
+    <input
+      id="charge2_location_y"
+      name="charge2_location_y"
+      type="number"
+      placeholder="1"
+      bind:value="{q2.location.y}" />
   </div>
 
   <div class="set_input_section">
@@ -90,52 +330,206 @@ const onInputSet = (input_value: string) => {
       name="charge3"
       type="number"
       placeholder="-1"
-      bind:value="{inputQ3}"
+      bind:value="{q3.value}"
       on:input="{onInputSet(inputQ3)}" />
   </div>
 
-  <div class="result">
-    |F| = {forceMagnitude}
-  </div>
+  <div class="set_input_section">
+    <span>Charge 3 location: </span>
+    <label for="charge3_location_x">x: </label>
+    <input
+      id="charge3_location_x"
+      name="charge3_location_x"
+      type="number"
+      placeholder="1"
+      bind:value="{q3.location.x}" />
 
-  <div class="drawing">
-    <svg
-      width="100%"
-      height="500"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg">
-      <defs>
-        <marker
-          id="arrowhead"
-          markerWidth="4"
-          markerHeight="4"
-          orient="auto"
-          refY="2">
-          <path d="M0,0 L4,2 0,4"></path>
-        </marker>
-      </defs>
+    <label for="charge3_location_y">y: </label>
+    <input
+      id="charge3_location_y"
+      name="charge3_location_y"
+      type="number"
+      placeholder="1"
+      bind:value="{q3.location.y}" />
 
-      <circle
-        cx="25"
-        cy="75"
-        r="20"
-        stroke="red"
-        fill="transparent"
-        stroke-width="5"></circle>
+    <div class="error">
+      {error}
+    </div>
 
-      <line x1="10" x2="50" y1="110" y2="150" stroke="orange" stroke-width="5"
-      ></line>
+    <div class="result">
+      |F12| = {F1on2}
+    </div>
 
-      <line
+    <div class="drawing">
+      <svg
+        width="100%"
+        height="{COORDINATES_SYSTEM_MAX_LENGTH * SCALE}"
+        style="transform: rotateX(180deg);"
+        version="1.1"
+        xmlns="http://www.w3.org/2000/svg">
+        <defs>
+          <marker
+            id="arrow-head-blue"
+            markerWidth="4"
+            markerHeight="4"
+            orient="auto"
+            refY="2">
+            <path d="M0,0 L4,2 0,4"></path>
+          </marker>
+        </defs>
+
+        <!-- Coodinate System -->
+        <!-- x -->
+        <text
+          x="98%"
+          y="{cartesianToSVG(0, COORDINATES_SYSTEM_MAX_LENGTH - 9.5).y}"
+          >x</text>
+
+        <line
+          x1="{cartesianToSVG(0, 0).x}"
+          y1="{cartesianToSVG(0, 0).y}"
+          x2="100%"
+          y2="{cartesianToSVG(COORDINATES_SYSTEM_MAX_LENGTH, 0).y}"
+          stroke="black"
+          stroke-width="5"></line>
+
+        <!-- y -->
+        <line
+          x1="0"
+          y1="0"
+          x2="{cartesianToSVG(0, COORDINATES_SYSTEM_MAX_LENGTH).x}"
+          y2="{cartesianToSVG(0, COORDINATES_SYSTEM_MAX_LENGTH).y}"
+          stroke="black"
+          stroke-width="5"></line>
+
+        <!-- Charges -->
+        <circle
+          cx="{cartesianToSVG(q1.location.x, q1.location.y).x}"
+          cy="{cartesianToSVG(q1.location.x, q1.location.y).y}"
+          r="{Math.abs(q1.value)}"
+          stroke="red"
+          fill="transparent"
+          stroke-width="5"></circle>
+
+        <circle
+          cx="{cartesianToSVG(q2.location.x, q2.location.y).x}"
+          cy="{cartesianToSVG(q2.location.x, q2.location.y).y}"
+          r="{Math.abs(q2.value)}"
+          stroke="blue"
+          fill="transparent"
+          stroke-width="5"></circle>
+
+        <circle
+          cx="{cartesianToSVG(q3.location.x, q3.location.y).x}"
+          cy="{cartesianToSVG(q3.location.x, q3.location.y).y}"
+          r="{Math.abs(q3.value)}"
+          stroke="red"
+          fill="transparent"
+          stroke-width="5"></circle>
+
+        <line
+          x1="{cartesianToSVG(
+            force(q1, q2).F2on1.p1.x,
+            force(q1, q2).F2on1.p1.y
+          ).x}"
+          y1="{cartesianToSVG(
+            force(q1, q2).F2on1.p1.x,
+            force(q1, q2).F2on1.p1.y
+          ).y}"
+          x2="{cartesianToSVG(
+            force(q1, q2).F2on1.p2.x,
+            force(q1, q2).F2on1.p2.y
+          ).x}"
+          y2="{cartesianToSVG(
+            force(q1, q2).F2on1.p2.x,
+            force(q1, q2).F2on1.p2.y
+          ).y}"
+          stroke="blue"
+          marker-color="blue"
+          marker-end="url(#arrow-head-blue)"
+          stroke-width="5"></line>
+
+        <line
+          x1="{cartesianToSVG(
+            force(q1, q2).F1on2.p1.x,
+            force(q1, q2).F1on2.p1.y
+          ).x}"
+          y1="{cartesianToSVG(
+            force(q1, q2).F1on2.p1.x,
+            force(q1, q2).F1on2.p1.y
+          ).y}"
+          x2="{cartesianToSVG(
+            force(q1, q2).F1on2.p2.x,
+            force(q1, q2).F1on2.p2.y
+          ).x}"
+          y2="{cartesianToSVG(
+            force(q1, q2).F1on2.p2.x,
+            force(q1, q2).F1on2.p2.y
+          ).y}"
+          stroke="blue"
+          marker-color="blue"
+          marker-end="url(#arrow-head-blue)"
+          stroke-width="5"></line>
+
+        <line
+          x1="{cartesianToSVG(
+            force(q1, q3).F2on1.p1.x,
+            force(q1, q3).F2on1.p1.y
+          ).x}"
+          y1="{cartesianToSVG(
+            force(q1, q3).F2on1.p1.x,
+            force(q1, q3).F2on1.p1.y
+          ).y}"
+          x2="{cartesianToSVG(
+            force(q1, q3).F2on1.p2.x,
+            force(q1, q3).F2on1.p2.y
+          ).x}"
+          y2="{cartesianToSVG(
+            force(q1, q3).F2on1.p2.x,
+            force(q1, q3).F2on1.p2.y
+          ).y}"
+          stroke="blue"
+          marker-color="blue"
+          marker-end="url(#arrow-head-blue)"
+          stroke-width="5"></line>
+
+        <line
+          x1="{cartesianToSVG(
+            force(q1, q3).F1on2.p1.x,
+            force(q1, q3).F1on2.p1.y
+          ).x}"
+          y1="{cartesianToSVG(
+            force(q1, q3).F1on2.p1.x,
+            force(q1, q3).F1on2.p1.y
+          ).y}"
+          x2="{cartesianToSVG(
+            force(q1, q3).F1on2.p2.x,
+            force(q1, q3).F1on2.p2.y
+          ).x}"
+          y2="{cartesianToSVG(
+            force(q1, q3).F1on2.p2.x,
+            force(q1, q3).F1on2.p2.y
+          ).y}"
+          stroke="blue"
+          marker-color="blue"
+          marker-end="url(#arrow-head-blue)"
+          stroke-width="5"></line>
+
+        <!-- <line
         x1="0"
         y1="0"
         x2="50"
         y2="50"
         stroke="#000"
         stroke-width="6"
-        marker-end="url(#arrowhead)"></line>
-    </svg>
+        marker-end="url(#arrowhead)"></line> -->
+      </svg>
+    </div>
   </div>
-
-  <!-- <canvas bind:this="{canvas}" width="{32}" height="{32}"></canvas> -->
 </main>
+
+<style>
+main {
+  @apply container mx-auto;
+}
+</style>
